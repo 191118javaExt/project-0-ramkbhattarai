8c42a43563bdb04af642eb04cb56966d73cca5c3
Ram <ramkb466@gmail.com>
2019-12-11 18:00:33 -0500

    completed connection class

--- src/main/java/com/revature/utils/ConnectionUtil.java
+++ src/main/java/com/revature/utils/ConnectionUtil.java
@@ -1,5 +1,29 @@
 package com.revature.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+
 public class ConnectionUtil {
+	
+	private static Logger logger = Logger.getLogger(ConnectionUtil.class);
+
+	public static Connection getConnection() {
+		
+		String url = "jdbc:postgresql://localhost:5432/";
+		String username = "postgres";
+		String password = "";
+		
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			logger.warn("Unable to obtain connection to database", e);
+		}
+		
+		return con;
+	}
 
 }
