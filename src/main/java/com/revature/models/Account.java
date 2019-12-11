e5e3acd58124260dc2817710e07cc056a6ae3c9b
Ram <ramkb466@gmail.com>
2019-12-11 17:55:54 -0500

    completed account class

--- src/main/java/com/revature/models/Account.java
+++ src/main/java/com/revature/models/Account.java
@@ -1,6 +1,8 @@
 package com.revature.models;
 
 public class Account {
+
+
 	private int id;
 	private String accountType;
 	private int accountNumber;
@@ -16,12 +18,72 @@ public class Account {
 		accountNumber = startingAccountNumber++;
 	}
 	
-	public Account(int id, double balance, String accountType, boolean isJoint) {
+	
+	
+
+
+
+
+	public Account(int id, String accountType, int accountNumber, double balance, double interestRate,
+			boolean isJoint) {
 		super();
 		this.id = id;
-		this.balance = balance;
 		this.accountType = accountType;
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.interestRate = interestRate;
 		this.isJoint = isJoint;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + accountNumber;
+		result = prime * result + ((accountType == null) ? 0 : accountType.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(balance);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + id;
+		temp = Double.doubleToLongBits(interestRate);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + (isJoint ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (accountNumber != other.accountNumber)
+			return false;
+		if (accountType == null) {
+			if (other.accountType != null)
+				return false;
+		} else if (!accountType.equals(other.accountType))
+			return false;
+		if (Double.doubleToLongBits(balance) != Double.doubleToLongBits(other.balance))
+			return false;
+		if (id != other.id)
+			return false;
+		if (Double.doubleToLongBits(interestRate) != Double.doubleToLongBits(other.interestRate))
+			return false;
+		if (isJoint != other.isJoint)
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", accountType=" + accountType + ", accountNumber=" + accountNumber + ", balance="
+				+ balance + ", interestRate=" + interestRate + ", isJoint=" + isJoint + "]";
 	}
 
 }
