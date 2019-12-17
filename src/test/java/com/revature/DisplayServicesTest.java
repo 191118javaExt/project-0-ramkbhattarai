6bd59deae52909ed07e26ce7a9cea565a9adb75d
Ram <ramkb466@gmail.com>
2019-12-17 14:11:28 -0500

    added classes for junit test

--- src/test/java/com/revature/DisplayServicesTest.java
+++ src/test/java/com/revature/DisplayServicesTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class AccountTest {
+public class DisplayServicesTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
